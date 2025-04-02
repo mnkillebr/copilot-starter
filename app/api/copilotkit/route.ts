444698b312385e635a521fbfd980c5273e1abc17
mnkillebr <killebrew.biz@gmail.com>
2025-04-02 15:51:56 -0400

    initial commit

---
+++ app/api/copilotkit/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest } from "next/server";
+import {
+  CopilotRuntime,
+  GroqAdapter,
+  OpenAIAdapter,
+  copilotRuntimeNextJSAppRouterEndpoint,
+} from "@copilotkit/runtime";
+
+// const serviceAdapter = new OpenAIAdapter({});
+const serviceAdapter = new GroqAdapter({ model: "llama-3.1-8b-instant" });
+
+const runtime = new CopilotRuntime({
+  remoteEndpoints: [
+    {
+      url: process.env.REMOTE_ACTION_URL || "http://localhost:8000/copilotkit",
+    },
+  ],
+});
+
+export const POST = async (req: NextRequest) => {
+  const { handleRequest } = copilotRuntimeNextJSAppRouterEndpoint({
+    runtime,
+    serviceAdapter,
+    endpoint: "/api/copilotkit",
+  });
+
+  return handleRequest(req);
+};
